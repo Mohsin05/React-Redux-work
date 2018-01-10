6e0f145cdddd0cac1ca4c2f92f34da24d8db82ed
mohsin05 <mohsinyounas05@gmail.com>
2018-01-10 16:58:49 +0100

    facebook/gmail authentication

--- assets/javascripts/Navbar/index.js
+++ assets/javascripts/Navbar/index.js
@@ -19,16 +19,30 @@ class Navbar extends Component {
     return (
       <nav className="cruddy-auth-blog-nav navbar navbar-fixed-top">
         <div className="navbar-header">
-          <span className="navbar-brand"><b>Inventory Blog</b></span>
+          <span className="navbar-brand">Inventory Blog</span>
         </div>
         <ul className="nav navbar-nav navbar-right">
-                <li className="nav-button">
+          <li className={`nav user-photo ${user && user.google && user.google.photo && 'show'}`}
+              style={user && user.google && user.google.photo && {backgroundImage: `url(${user.google.photo})`}}/>
+          <li className={`nav user-photo ${user && user.facebook && user.facebook.photo && 'show'}`}
+              style={user && user.facebook && user.facebook.photo && {backgroundImage: `url(${user.facebook.photo})`}}></li>
+          <li className="nav-button">
             {
-
+              (!user || !user.email || !user.hasPassword || !user.google || !user.google.photo || !user.facebook || !user.facebook.photo)
               &&
               <span>
                 LOG IN &#10161;
-
+                {
+                  (!user || !user.google)
+                  &&
+                  <a href="/auth/google"><i className="fa fa-google o-auth-btn"></i></a>
+                }
+                {
+                  (!user || !user.facebook)
+                  &&
+                  <a href="/auth/facebook"><i className="fa fa-facebook o-auth-btn"></i></a>
+                }
+                {
                   (!user || !user.email)
                   &&
                   <input className="nav-input" ref="email" placeholder="email" type="text"/>
@@ -42,9 +56,9 @@ class Navbar extends Component {
                 {
                   (!user || !user.hasPassword)
                   &&
-                  <button className="local-auth-button" onClick={this.handleLocalAuth.bind(this)}>Post LocalAuth</button>
+                  <button className="local-auth-button" onClick={this.handleLocalAuth.bind(this)}>Login</button>
                 }
-         
+              </span>
             }
             {
               user
